48e536c8d8a3e50479ea68fe461d9dbdfa1612af
bal16 <ahmadhamidbalya@gmail.com>
2026-08-05 12:28:50 +0700

    refactor(rename): for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
-import PWAOptions from "./src/VitePWAOptions";
+import PWAOptions from "./src/vite-pwa-options";
 
 // https://vite.dev/config/
 export default defineConfig({
